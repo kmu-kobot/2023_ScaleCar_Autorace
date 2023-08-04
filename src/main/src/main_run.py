7ada17f5bcb4c794f5db1f6fd2a148da7d86f173
ChaNeeeeeee <105336883+ChaNeeeeeee@users.noreply.github.com>
2023-08-04 12:07:18 +0900

    add rubbercon algorithm
    +rubbercon node
    +rubbercon misson on main node

--- src/main/src/main_run.py
+++ src/main/src/main_run.py
@@ -33,6 +33,10 @@ class MainLoop:
         self.slow_t1 = 0.0
         self.sign_data = 0
 
+        # for rubbercon misson
+        self.is_rubbercon_mission = False
+        self.rubbercon_angle_error = 0
+
         rospy.Timer(rospy.Duration(1.0/30.0), self.timerCallback)
         self.webot_speed_pub = rospy.Publisher("/commands/motor/speed", Float64, queue_size=1) # motor speed
         self.webot_angle_pub = rospy.Publisher("/commands/servor/position", Float64, queue_size=1) # servo angle
@@ -108,7 +112,11 @@ class MainLoop:
     
 
     def rubbercone_callback(self, _data):
-        pass
+        self.rubbercon_angle_error = _data.data
+        if self.rubbercon_angle_error < 10.0 :
+            self.is_rubbercon_mission = True
+        else :
+            self.is_rubbercon_mission = False
 
 
     def mainAlgorithm(self):
@@ -138,14 +146,31 @@ class MainLoop:
                     t2 = rospy.get_time()
                 self.slow_down_flag = 0
                 self.slow_flag = 0
+        
+        # rubbercon mission
+        elif self.is_rubbercon_mission == True:
+            self.rubbercon_drive()
+            self.current_lane = "RIGHT"
 
-        #defalut driving
+        # defalut driving
         else:
             speed_msg.data = 1000 # defalut speed
             angle_msg.data = self.slide_x_location - 0.165 # calculate angle error with slingwindow data (!!tuning required!!)
             self.webot_speed_pub.publish(speed_msg) # publish speed
             self.webot_angle_pub.publish(angle_msg) # publish angle
+
+    def rubbercon_drive(self) :
+        rospy.loginfo("rubbercon_drive!!!!!!!!!!!!!!!!!")
+        speed_msg = Float64() # speed msg create
+        angle_msg = Float64() # angle msg create
         
+        speed_msg.data = 500 # rubbercon speed
+        angle_msg.data = self.rubbercon_angle_error * -1.0
+
+        self.webot_speed_pub.publish(speed_msg) # publish speed
+        self.webot_angle_pub.publish(angle_msg) # publish angle
+        
+
 def nothing(x):
     pass
 
